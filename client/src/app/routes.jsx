a906eef6cf7363dd8f12557d8d92ad74a5f25ea6
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-02-27 07:18:09 +0530

    Update routes.jsx

--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -1,23 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// layout
+import Navbar from "../components/layout/Navbar";
+
+// pages
+import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
 import Profile from "../pages/Profile/Profile";
 import Matches from "../pages/Dating/Matches";
-
+import Random from "../pages/Random/Random";
+import Dating from "../pages/Dating/Dating";
+import Friendship from "../pages/Friendship/Friendship";
+import Marriage from "../pages/Marriage/Marriage";
+import Remarriage from "../pages/Remarriage/Remarriage";
+import Talk from "../pages/Talk/Talk";
+import Chat from "../pages/Chat/Chat";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
-        <Route path="/remarriage" element={<Remarriage />} />
-        <Route path="/marriage" element={<Marriage />} />
-        <Route path="/friendship" element={<Friendship />} />
-        <Route path="/talk" element={<Talk />} />
-        <Route path="/random" element={<Random />} />
-        <Route path="/matches" element={<Matches />} />
-<Route path="/profile" element={<Profile />} />
-        <Route path="/dating" element={<Dating />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
+        {/* main */}
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
+
+        {/* modes */}
+        <Route path="/random" element={<Random />} />
+        <Route path="/dating" element={<Dating />} />
+        <Route path="/friendship" element={<Friendship />} />
+        <Route path="/marriage" element={<Marriage />} />
+        <Route path="/remarriage" element={<Remarriage />} />
+        <Route path="/talk" element={<Talk />} />
+
+        {/* social */}
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+
+        {/* user */}
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
