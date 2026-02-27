81b7ac5ea8270c5e740ad2f5ec3f72c44c88c817
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-02-27 07:01:57 +0530

    Update index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,3 +2,5 @@ exports.likeUser = require("./api/dating").likeUser;
 exports.sendMessage = require("./api/chat").sendMessage;
 exports.joinRandom = require("./api/random").joinRandom;
 exports.joinTalk = require("./api/talk").joinTalk;
+exports.addFriend = require("./api/friendship").addFriend;
+exports.acceptFriend = require("./api/friendship").acceptFriend;
