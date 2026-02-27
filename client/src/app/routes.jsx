3989e3d803bd21df2750512192953e1240bd9160
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-02-27 07:12:55 +0530

    Update routes.jsx

--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -8,6 +8,7 @@ export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/remarriage" element={<Remarriage />} />
         <Route path="/marriage" element={<Marriage />} />
         <Route path="/friendship" element={<Friendship />} />
         <Route path="/talk" element={<Talk />} />
