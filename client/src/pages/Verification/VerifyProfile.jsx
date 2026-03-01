28405780ba159aa5105cf63b5e670f7e420b2909
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 17:09:29 +0530

    Create VerifyProfile.jsx

---
+++ client/src/pages/Verification/VerifyProfile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function VerifyProfile() {
+  return (
+    <div className="verification">
+      <h1>Verify Your Profile</h1>
+      <p>Increase trust and visibility</p>
+    </div>
+  );
+}
