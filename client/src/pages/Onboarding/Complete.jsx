d5da70d187431bf9e464c159cab2396bef8a177e
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 17:08:44 +0530

    Create Complete.jsx

---
+++ client/src/pages/Onboarding/Complete.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Complete() {
+  return (
+    <div className="onboarding">
+      <h1>Profile Complete 🎉</h1>
+      <p>You’re ready to start matching</p>
+    </div>
+  );
+}
