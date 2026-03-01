3c119e97d9e896cce7c5949e9171c36c5c7c942c
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 16:59:26 +0530

    Create Reports.jsx

---
+++ client/src/pages/Admin/Reports.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminReports() {
+  return (
+    <div className="admin-page">
+      <h1>Reports</h1>
+      <p>User reports & moderation</p>
+    </div>
+  );
+}
