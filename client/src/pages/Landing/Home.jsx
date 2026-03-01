5716c66688a7ebe8e417e139ff5716b77cec0e2e
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 17:16:05 +0530

    Create Home.jsx

---
+++ client/src/pages/Landing/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="landing">
+      <h1>Dilset</h1>
+      <p>Jahan Dil Set Ho ❤️</p>
+    </div>
+  );
+}
