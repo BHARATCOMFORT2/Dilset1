4f5b66e6c0c386ec649f570f1dadc8aa877cf50b
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 17:10:58 +0530

    Create Swipe.jsx

---
+++ client/src/pages/Swipe/Swipe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Swipe() {
+  return (
+    <div className="swipe-page">
+      <h1>Discover</h1>
+      <p>Swipe to match</p>
+    </div>
+  );
+}
