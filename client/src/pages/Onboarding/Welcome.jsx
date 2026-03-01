90c9e533a955a02dc8140423ab33c69715915dac
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 17:05:11 +0530

    Create Welcome.jsx

---
+++ client/src/pages/Onboarding/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <div className="onboarding">
+      <h1>Welcome to Dilset</h1>
+      <p>Let’s create your profile</p>
+    </div>
+  );
+}
