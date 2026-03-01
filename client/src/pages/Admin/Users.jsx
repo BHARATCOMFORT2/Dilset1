438a3491a294e24cd4abc40507819ead748b1ad6
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-03-01 16:58:58 +0530

    Create Users.jsx

---
+++ client/src/pages/Admin/Users.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminUsers() {
+  return (
+    <div className="admin-page">
+      <h1>Users</h1>
+      <p>Manage all users</p>
+    </div>
+  );
+}
